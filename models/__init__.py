86da680531b59b38cd162843e3c1116005f77735
loyd ngei <loydngei@gmail.com>
2023-02-11 13:00:29 +0300

    engine

---
+++ models/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Importing Filestorage"""
+
+from models.engine.filestorage import FileStorage
+
+storage = FileStorage()
+storage.reload()
--- models/base_model.py
+++ models/base_model.py
@@ -2,24 +2,45 @@
 
 import uuid
 from datetime import datetime, time, date
+from models import storage
 
 
 class BaseModel:
     """Define the class BaseModel that defines attributes id,
     created_at, updated_at and methods
     """
-    
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initialize the class BaseModel attributes{id, created_at
         and updated_at}
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            storage.new(self)
+        else:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    if key in ["created_at", "updated_at"]:
+                        value = datetime.\
+                                strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, key, value)
 
     def __str__(self):
         """Returns a str representation of the class"""
         return (f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
 
+    def save(self):
+        """Update the updated_at attr"""
+        self.updated_at = datetime.now()
+        storage.save()
 
-
+    def to_dict(self):
+        """Returns a dictionary representation of the instance"""
+        my_dict = self.__dict__.copy()
+        my_dict.update({
+            "__class__": self.__class__.__name__,
+            "updated_at": self.updated_at.isoformat(),
+            "created_at": self.created_at.isoformat()
+            })
+        return my_dict
